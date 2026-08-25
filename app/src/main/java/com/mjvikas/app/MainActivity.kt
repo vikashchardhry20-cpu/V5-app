c6519909d3e5749ad0737253f42f26f74670be20
vikashchardhry20-cpu <vikashchardhry20@gmail.com>
2026-08-25 22:10:43 +0530

    Add MainActivity with WebView implementation

---
+++ app/src/main/java/com/mjvikas/app/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.mjvikas.app
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        webView = WebView(this)
+        setContentView(webView)
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://github.com/vikashchardhry20-cpu")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
+
