1884ee5972fa5b1010c278143f3a288e0591096b
vikashchardhry20-cpu <vikashchardhry20@gmail.com>
2026-08-25 11:08:20 +0530

    Add MainActivity with a simple composable screen

---
+++ app/src/main/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.mjvikas.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AppScreen()
+        }
+    }
+}
+
+@Composable
+fun AppScreen() {
+    Text(text = "MJ विकास")
+}
